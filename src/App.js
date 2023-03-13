276013cf08c9af0e63fd8b405b0b4354b953fa71
Qmun14 <mamunramdhan@gmail.com>
2023-03-13 15:50:27 +0700

    Membuat component FormAddUser, FormEditUser dan membuat halaman nya

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import AddUser from "./pages/AddUser";
 import Dashboard from "./pages/Dashboard";
+import EditUser from "./pages/EditUser";
 import Products from "./pages/Products";
 import Users from "./pages/Users";
 
@@ -12,6 +14,8 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
           <Route path="/users" element={<Users />} />
+          <Route path="/users/add" element={<AddUser />} />
+          <Route path="/users/edit/:id" element={<EditUser />} />
           <Route path="/products" element={<Products />} />
         </Routes>
       </BrowserRouter>
---
+++ src/components/FormEditUser.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+
+const FormEditUser = () => {
+    return (
+        <div className="bg-CDS_N-2 w-full h-screen flex items-center">
+            <div className="container flex max-w-full items-center h-full justify-center">
+                <div className="bg-CDS_2  py-5 px-3 rounded-xl shadow-lg font-roboto w-3/4 flex flex-col items-center justify-center " >
+                    <h1 className='font-bold text-3xl'>User</h1>
+                    <h2 className='text-xl mb-1'>Edit User</h2>
+                    <div className="flex-col relative flex  w-full">
+                        <form action="" onSubmit={''} className="mb-8 mt-3  mx-8">
+                            <p className="text-center text-red-400 text-sm mt-1 font-semibold mb-5">{''}</p>
+                            <div className="flex-1 my-3 justify-start font-semibold">
+                                <label htmlFor="name">
+                                    Name
+                                </label>
+                            </div>
+                            <div className="mt-3 mb-5">
+                                <input type="text" id="user" placeholder="name" className="h-12 text-lg w-full   px-4 rounded-md outline-blue-500" autoFocus
+                                    value={''} onChange={'(e) => setName(e.target.value)'} />
+                            </div>
+                            <div className="flex-1 my-3 justify-start font-semibold">
+                                <label htmlFor="email">
+                                    Email
+                                </label>
+                            </div>
+                            <div className="mt-3 mb-5">
+                                <input type="email" id="email" placeholder="Email" className="h-12 text-lg w-full   px-4 rounded-md outline-blue-500"
+                                    value={''} onChange={'(e) => setEmail(e.target.value)'} />
+                            </div>
+                            <div className="flex-1 my-3 justify-start font-semibold">
+                                <label htmlFor="password">
+                                    Password
+                                </label>
+                            </div>
+                            <div className="mt-3 mb-5 ">
+                                <input type="password" id="password" placeholder="******" className=" h-12 text-lg w-full   px-4 rounded-md outline-blue-500"
+                                    value={''} onChange={'(e) => setPassword(e.target.value)'} />
+                            </div>
+                            <div className="flex-1 my-3 justify-start font-semibold">
+                                <label htmlFor="confirm_password">
+                                    Confirm Password
+                                </label>
+                            </div>
+                            <div className="mt-3 mb-5">
+                                <input type="password" id="confirm_password" placeholder="******" className=" h-12 text-lg w-full   px-4 rounded-md outline-blue-500"
+                                    value={''} onChange={"(e) => setConfPassword(e.target.value)"} />
+                            </div>
+                            <div className="flex-1 my-3 justify-start font-semibold">
+                                <label htmlFor="role">
+                                    Role
+                                </label>
+                            </div>
+                            <div className="mt-3 mb-5">
+                                <select className='w-full h-12 text-lg px-4 rounded-md'>
+                                    <option value="admin" className='h-10'>Admin</option>
+                                    <option value="user" className='h-10'>User</option>
+                                </select>
+                            </div>
+                            <div className='flex justify-center'>
+                                <button className="bg-green-400 py-3 px-24 mt-5 w-1/2 rounded-md justify-items-center font-bold hover:bg-opacity-70">Update</button>
+                            </div>
+                        </form>
+                    </div>
+                    {/* <div className="flex mt-0 px-7 justify-center items-center text-base">
+                        <NavLink to={''} className="hover:text-blue-500">Kembali</NavLink>
+                    </div> */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FormEditUser;
