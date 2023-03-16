b3edbec5f9f38866d85182d503c5e5d0bd50147a
Qmun14 <mamunramdhan@gmail.com>
2023-03-16 10:18:01 +0700

    Membuat fitur saveProduct

--- src/api/products.js
+++ src/api/products.js
@@ -4,6 +4,11 @@ import { config } from '../config/index';
 export const getProducts = () => {
     return Axios.get(`${config.api_url}/products`);
 }
+
+export const addProduct = (data) => {
+    return Axios.post(`${config.api_url}/products`, data);
+}
+
 export const deleteProduct = (id) => {
     return Axios.delete(`${config.api_url}/products/${id}`);
 }
--- src/components/FormAddProduct.jsx
+++ src/components/FormAddProduct.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { addProduct } from '../api/products';
 
 const FormAddProduct = () => {
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [message, setMessage] = useState('');
+    const navigate = useNavigate();
+
+    const saveProduct = async (e) => {
+        e.preventDefault();
+        const payload = {
+            name: name, price: price
+        }
+        try {
+            await addProduct(payload);
+            navigate('/products');
+        } catch (err) {
+            if (err.response) {
+                setMessage(err.response.data.message);
+            }
+        }
+    }
+
+
     return (
         <div className="bg-CDS_N-2 w-full h-screen flex items-center">
             <div className="container flex max-w-full items-center h-full justify-center">
@@ -8,8 +31,8 @@ const FormAddProduct = () => {
                     <h1 className='font-bold text-3xl'>Product</h1>
                     <h2 className='text-xl mb-1'>Add New Product</h2>
                     <div className="flex-col relative flex  w-full">
-                        <form action="" onSubmit={''} className="mb-8 mt-3  mx-8">
-                            <p className="text-center text-red-400 text-sm mt-1 font-semibold mb-5">{''}</p>
+                        <form onSubmit={saveProduct} className="mb-8 mt-3  mx-8">
+                            <p className="text-center text-red-400 text-sm mt-1 font-semibold mb-5">{message}</p>
                             <div className="flex-1 my-3 justify-start font-semibold">
                                 <label htmlFor="name">
                                     Name
@@ -17,7 +40,7 @@ const FormAddProduct = () => {
                             </div>
                             <div className="mt-3 mb-5">
                                 <input type="text" id="name" placeholder="Product name" className="h-12 text-lg w-full   px-4 rounded-md outline-blue-500" autoFocus
-                                    value={''} onChange={'(e) => setName(e.target.value)'} />
+                                    value={name} onChange={(e) => setName(e.target.value)} />
                             </div>
                             <div className="flex-1 my-3 justify-start font-semibold">
                                 <label htmlFor="price">
@@ -26,10 +49,10 @@ const FormAddProduct = () => {
                             </div>
                             <div className="mt-3 mb-5">
                                 <input type="number" id="price" placeholder="0" className="h-12 text-lg w-full   px-4 rounded-md outline-blue-500"
-                                    value={''} onChange={'(e) => setEmail(e.target.value)'} />
+                                    value={price} onChange={(e) => setPrice(e.target.value)} />
                             </div>
                             <div className='flex justify-center'>
-                                <button className="bg-green-400 py-3 px-24 mt-5 w-1/2 rounded-md justify-items-center font-bold hover:bg-opacity-70">Save</button>
+                                <button className="bg-green-400 py-3 px-24 mt-5 w-1/2 rounded-md justify-items-center font-bold hover:bg-opacity-70" type='submit'>Save</button>
                             </div>
                         </form>
                     </div>
